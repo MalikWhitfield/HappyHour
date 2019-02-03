4ca26005bfdd84a099941b6be8823161c2b7e7d6
Malik Whitfield <malikwhitfield29@gmail.com>
2019-02-02 19:29:06 -0700

    notes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home container-fluid">
     <navbar></navbar>
+    <!-- GET RID OF THE GUTTER ON THE LEFT -->
     <h1>Happy Hour!</h1>
     <i class="fas fa-cocktail fa-4x"></i>
   </div>
@@ -8,7 +9,7 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/HelloWorld.vue"; //Figure out how to make this work without Helloworld
 import Navbar from "@/components/Navbar.vue";
 export default {
   name: "home",
