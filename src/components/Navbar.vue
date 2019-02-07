896b5f8d3824953d29d11f99db04a1c6afcdc931
Malik Whitfield <malikwhitfield29@gmail.com>
2019-02-06 21:15:50 -0700

    burger

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,48 @@
 <template>
-    <div class="container-fluid">
-        <nav class="navbar navbar-light bg-light justify-content-between" style="width: 100vw">
-            <i class="fas fa-cocktail"></i>
-            <a class="navbar-brand">My List</a>
-            <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </nav>
-    </div>
+  <div class="container-fluid">
+    <nav class="navbar navbar-light bg-light justify-content-between" style="width: 100vw">
+      <i class="fas fa-cocktail"></i>
+      <a class="navbar-brand">My List</a>
+      <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+        <!-- Links -->
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+        <!-- Links -->
+      </div>
+    </nav>
+    <!-- Collapsible content -->
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Navbar',
-        data() {
-            return {
-
-            }
-        },
-        computed: {},
-        methods: {}
-    }
-
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {}
+};
 </script>
 
 <style>
-    .logo-size {
-        width: 250px;
-        height: 250px;
-    }
+.logo-size {
+  width: 250px;
+  height: 250px;
+}
 </style>
